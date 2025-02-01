f3e22b934a951a8da8269fa405651258e097c2a6
Igor Carvalho <igorcarvalho2907@gmail.com>
2025-01-31 21:09:51 -0300

    reinicialização

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const precoAtual = 100;
+const precoAnterior = 80;
+const limiteDePreco = 110;
+
+function teste(precoAnterior, precoAtual){
+    if(precoAnterior<precoAtual) {
+        return 'Promoção';
+    }else {
+        return 'sem promoção';
+    }
+}
